09d1e9ae2d8bf86dec560ec31fd1023724bb0478
kgr2000 <garmgoon@gmail.com>
2020-04-18 11:28:51 +0900

    create git

---
+++ js_tutorial/practice_02_04.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// strict mode 에서는 변수 선언 시 꼭 let이나 const가 들어가야 함
+num = 5; //error 발생
+let num = 5; //정상
---
+++ js_tutorial/practice_02_07.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//거듭제곱 연산자
+let expo = 2 ** 3;
+function foo() {
+  document.getElementById("setArea").innerHTML = `let expo = 2 ** 3; -> ${expo}`;
+}
+foo();
