9ae9798c54ef56ee5a9a70827e1e882938011941
kgr2000 <garmgoon@gmail.com>
2020-04-28 17:46:19 +0900

    seo-blog created

---
+++ react/seoblog/backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// bring routes
+const blogRoutes = require("./routes/blog");
+
+// app
+const app = express();
+
+// database connection
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// cors
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+// routes middleware
+app.use("/api", blogRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port${port}`);
+});
